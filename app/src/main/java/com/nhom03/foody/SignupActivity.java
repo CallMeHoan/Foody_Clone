7d8429242d481d6d3a5cee9de69e25478b5f9b6b
ThienHoanNeee <softteam2021@gmail.com>
2022-04-20 10:25:51 +0700

    [Update] update directory for signin and signup page

--- app/src/main/java/com/nhom03/foody/SignupActivity.java
+++ app/src/main/java/com/nhom03/foody/SignupActivity.java
@@ -1,26 +1,29 @@
 package com.nhom03.foody;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class SignupActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    TextView signin_txt;
     TextView signup_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
+        signin_txt = (TextView) findViewById(R.id.signin_tv);
         signup_txt =  (TextView) findViewById(R.id.signup_tv);
 
-        signup_txt.setOnClickListener(new View.OnClickListener() {
+        signin_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login_page = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(login_page);
+                Intent signup_page = new Intent(SignupActivity.this, SigninActivity.class);
+                startActivity(signup_page);
             }
         });
     }
